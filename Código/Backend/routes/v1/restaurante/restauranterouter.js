1b529dee72d551ef4e9b690c2e182cce4e2c86c3
Fernando <ferestga8@gmail.com>
2024-04-06 13:41:00 +0200

    Editados permisos para tener la info del restaurante

--- Código/Backend/routes/v1/restaurante/restauranterouter.js
+++ Código/Backend/routes/v1/restaurante/restauranterouter.js
@@ -6,7 +6,7 @@ import asyncMiddleware from "middleware-async"
 
 
 const restauranteRouter = Router()
-restauranteRouter.get("/:restauranteId", asyncMiddleware(getRestaurante))
+restauranteRouter.get("/:restauranteId", permissionController("cocinero"), asyncMiddleware(getRestaurante))
 restauranteRouter.get("/", permissionController("superadmin"), asyncMiddleware(getRestaurante))
 restauranteRouter.post("/", permissionController("superadmin"), asyncMiddleware(createRestaurante))
 restauranteRouter.delete("/:restauranteId", permissionController("superadmin"), asyncMiddleware(deleteRestaurante))
