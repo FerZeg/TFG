5ac05e4eb8456598f2b0a7e75916e138ec30a348
Fernando <ferestga8@gmail.com>
2024-04-08 22:11:45 +0200

    Fixed bug

---
+++ Código/Backend/eslint.config.js
@@ -0,0 +1,86 @@
+import js from "@eslint/js"
+
+export default [
+	js.configs.recommended,
+	{
+
+
+
+		rules: {
+
+
+
+			"no-unused-vars": "warn",
+
+
+
+			"no-undef": "warn",
+
+
+
+			indent: [
+
+
+
+				"error",
+				"tab"
+			],
+			"linebreak-style": [
+				"error",
+				"windows"
+
+
+
+			],
+
+
+
+			quotes: [
+
+
+
+				"error",
+
+
+
+				"double"
+
+
+
+			],
+
+
+
+			semi: [
+
+
+
+				"error",
+
+
+
+				"never"
+
+
+
+			]
+
+
+
+		}
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+]
