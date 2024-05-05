6de3b0dcfff8127f156ec9e1e033cb5d58e2c06c
Fernando <ferestga8@gmail.com>
2024-05-05 17:23:02 +0200

    Conectar R2 cloudflare

--- Código/Backend/server.js
+++ Código/Backend/server.js
@@ -1,5 +1,26 @@
 import app from "./index.js"
 import { connectDB } from "./connection.js"
+import {
+	S3Client,
+	ListBucketsCommand,
+	ListObjectsV2Command,
+	GetObjectCommand,
+	PutObjectCommand
+} from "@aws-sdk/client-s3"
+
+const S3 = new S3Client({
+	region: "auto",
+	endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+	credentials: {
+	  accessKeyId: ACCESS_KEY_ID,
+	  secretAccessKey: SECRET_ACCESS_KEY,
+	},
+})
+console.log(
+	await S3.send(
+	  new ListBucketsCommand("")
+	)
+)  
 app.listen(process.env.PORT, () => {
 	console.log("El servidor está inicializado en el puerto " + process.env.PORT)
 	connectDB()
