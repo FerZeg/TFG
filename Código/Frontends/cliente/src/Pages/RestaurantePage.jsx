f294880cdf7a76a6532e47aa77ef07fcc8b0aaad
Fernando <ferestga8@gmail.com>
2024-05-27 00:05:56 +0200

    Arreglado bug carrito

--- Código/Frontends/cliente/src/Pages/RestaurantePage.jsx
+++ Código/Frontends/cliente/src/Pages/RestaurantePage.jsx
@@ -2,19 +2,27 @@ import Navigation from "../components/Navigation/Navigation"
 import Main from "../components/Main/Main"
 import Finish from "../components/Finish"
 import { useEffect } from "react"
-import { useRestauranteContext, useLoginContext } from "../lib/context"
-import { fetchProductos } from "../lib/fetchers"
+import { useRestauranteContext, useLoginContext, useTicketContext } from "../lib/context"
+import { fetchProductos, fetchTicket } from "../lib/fetchers"
 
 export default function RestaurantePage() {
     const { setProductos } = useRestauranteContext()
     const { login } = useLoginContext()
+    const { setTicket } = useTicketContext()
     useEffect(() => {
         fetchProductos(login.data).then(
             data => {
                 setProductos(data)
             }
         )
-    }, [setProductos, login])
+    }, [setProductos, login, setTicket])
+    useEffect(() => {
+        fetchTicket().then((val) => {
+            if(!val) {
+                setTicket(null)
+            }
+        })
+    }, [setTicket])
     return (
         <div className="main-container">
             <Navigation />
